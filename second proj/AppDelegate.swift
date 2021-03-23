218194ce9107e084c5f5be5854ca335823e6fe2f
Abdulaziz Yesuf <abdulazizyesuf7@gmail.com>
2021-03-23 18:48:10 +0300

    Initial Commit

---
+++ second proj/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  second proj
+//
+//  Created by Abdulaziz Yesuf Asmare on 3/23/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
